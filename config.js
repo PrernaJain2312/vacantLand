c8fb9127c48a6c87e1c0f15901737a530d2d77ee
Prerna Jain <you@example.com>
2019-03-02 18:29:57 +0530

    update

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ const PORT = process.env.PORT || 4000;
 
 const DATABASE_URL = process.env.DATABASE_URL || "mysql://vacantLandUser:vacantLandPass@localhost:3306/vacantLanddb";
 
-const SECRET_ACCESS_KEY =  "your key";
+const SECRET_ACCESS_KEY = "your key";
 
 const ACCESS_KEY_ID = "your key";
 
